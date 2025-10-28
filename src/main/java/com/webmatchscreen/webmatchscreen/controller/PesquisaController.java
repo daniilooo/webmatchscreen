2ab776e0e6fa97732bb7edc8591359c7d411c374
danilofranco-tigolden <danilo.franco@tigolden.com.br>
2025-10-28 14:32:47 -0300

    Refatoração da classe ApiServices
    Refatoração deixando o nível de acomplamento mais baixo, facilitando a manutenção e criação de novos endpoints.

--- src/main/java/com/webmatchscreen/webmatchscreen/controller/PesquisaController.java
+++ src/main/java/com/webmatchscreen/webmatchscreen/controller/PesquisaController.java
@@ -1,12 +1,16 @@
 package com.webmatchscreen.webmatchscreen.controller;
 
 import com.webmatchscreen.webmatchscreen.model.Titulo;
+import com.webmatchscreen.webmatchscreen.record.TemporadaRecord;
 import com.webmatchscreen.webmatchscreen.record.TituloRecord;
 import com.webmatchscreen.webmatchscreen.service.ApiServices;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/pesquisa")
@@ -19,9 +23,24 @@ public class PesquisaController {
     }
 
     @GetMapping("/{titulo}")
-    public Titulo buscarPorTitulo(@PathVariable String titulo) {
-        return apiServices.buscarTitulo(titulo);
+    public TituloRecord buscarPorTitulo(@PathVariable String titulo) {
+
+        Map<String, String> parametros = new HashMap<>();
+        parametros.put("titulo", titulo);
+
+        return apiServices.pesquisar(parametros, TituloRecord.class);
     }
+
+    @GetMapping("/{titulo}/{temporada}")
+    public TemporadaRecord buscarPortemporada(@PathVariable String titulo,
+                                              @PathVariable String temporada){
+        Map<String, String> parametros = new HashMap<>();
+        parametros.put("titulo", titulo);
+        parametros.put("temporada", temporada);
+
+        return apiServices.pesquisar(parametros, TemporadaRecord.class);
+    }
+
 
 
 }
